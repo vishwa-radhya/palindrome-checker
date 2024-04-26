995176b566a0c100b8eed478a10d09874fcdf788
vishwa-radhya <vishuvishwa735@gmail.com>
2024-04-26 15:14:39 +0530

    fixed the problrmatic code

--- script.js
+++ script.js
@@ -1,69 +1,31 @@
-const textInput = document.getElementById("text-input");
-const checkButton = document.getElementById("check-btn");
-const result = document.getElementById("result");
-const container = document.getElementById('main-f');
-let val = '';
-let filteredInput = '';
+const userInput = document.getElementById('text-input');
+const submitButton = document.getElementById('check-btn');
+const result = document.getElementById('result');
 
-
-  
-
-textInput.addEventListener('keyup',function(){
-    val = this.value;
-    filteredInput = filterInput(val);
-    checkPali(filteredInput,val);
+function checkPalindrome(input){
+    const orgInput = input;
+    if(input === ''){
+        alert('please enter some characters');
+        return;
+    }
+    result.replaceChildren();
+    let regex = /[^A-Z0-9a-z]/gi;
+    let newString = input.replace(regex,'').toLowerCase();
+    let word = newString === [...newString].reverse().join() ? 'is' : 'is not';
+    const HTMLString = `<strong>${orgInput}</strong> ${word} a palindrome.`;
+    const pTag = document.createElement('p');
+    pTag.className = 'output';
+    pTag.innerHTML = HTMLString;
+    result.appendChild(pTag);
+    result.classList.remove('hidden');
+}
+submitButton.addEventListener('click',()=>{
+    checkPalindrome(userInput.value);
+    userInput.value ='';
 });
-function filterInput(input){
-    
-    checkButton.addEventListener('click',()=>{
-        if(input === ''){
-            alert('please enter some characters');
-        }
-    })
-    let trimmer = input.trim();
-    
-    let regex = /[a-zA-Z0-9]/;
-    let rtn ='';
-    for(let i=0;i<trimmer.length;i++){
-        if(regex.test(trimmer[i])){
-            rtn+=trimmer[i];
-        }
-    }
-    return rtn;
-}
-function checkPali(input,val){
-    let flag =0; //tru
-    for(let i=0,j=input.length-1;i<input.length;i++,j--){
-        if(input[i]!==input[j]){
-            flag =1; //fal
-        }
-    }
-    let word = flag ==0 ? 'a palindrome' : 'not a palindrome';
-    const HTMLString = `<p><strong style="color:ghostwhite">${val}</strong> is ${word}.</p>`;
-    checkButton.addEventListener('click',()=>{
-        container.style.height = "11em";
-        result.innerHTML = HTMLString;
-        textInput.value ='';
-    })
-    
-}
-checkButton.addEventListener('click',()=>{
-    if(val === ''){
-    alert('please input some characters');
-    textInput.replaceChildren();
+userInput.addEventListener('keydown',e=>{
+    if(e.key === 'Enter'){
+        checkPalindrome(userInput.value);
+    userInput.value ='';
     }
 })
-
-// checkButton.addEventListener("click.empty", function() {
-//     if (textInput.value === "") {
-//       alert("Please enter a word or phrase to check.");
-//       checkButton.removeEventListener("click.empty"); // Remove temporary listener
-//     }
-//   }, { once: true });
-
-// userInput.addEventListener('keydown', e => {
-//     if (e.key === 'Enter') {
-//       checkForPalindrome(userInput.value);
-//       userInput.value = '';
-//     }
-//   });
